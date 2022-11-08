8225d0497999e0b13d36b5e51611fc53f269a937
Hayeon Lee <formahappy@gmail.com>
2022-11-08 11:52:35 +0900

    First Commit

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+
+	public static void main(String[] args )
+[[A[B[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C {
+	System.out.println("Helloworld");
+    }
+}
---
+++ Himedia.java
@@ -0,0 +1,8 @@
+public class Himedia {
+    public static void main(String[]arg) {
+
+	System.out.println("Hello Himedia");
+	System.out.println("Hello Class");
+
+    }
+}
